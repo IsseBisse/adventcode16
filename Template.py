527e1f0e8443ce4fbbddf727d2dd8c431d1347de
Liljequist Isak <isak.Liljequist@billerudkorsnas.com>
2019-07-05 11:49:38 +0200

    1-6 done! 7 started

---
+++ Template.py
@@ -0,0 +1,28 @@
+'''
+Input
+'''
+def parseInput(filename):
+	f = open(filename, "r")
+	data = f.read()
+
+	data = data.split(", ")
+
+	return data
+
+'''
+Part One
+'''
+def partOne():
+	a = 1
+
+'''
+Part Two
+'''
+def partTwo():
+	a = 1
+
+'''
+Main
+'''
+if __name__ == '__main__':
+	partOne()
